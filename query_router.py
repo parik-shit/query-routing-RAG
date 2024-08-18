dc970fcac893d46b8c71550f4bf8c558e124cebf
parik-shit <parikshitnasa@yahoo.in>
2024-08-18 13:14:13 +0530

    initialized the router

--- query_router.py
+++ query_router.py
@@ -1,11 +1,11 @@
 class QueryRouter:
-    def __init__(self, user_manual_embeddings, release_notes_embeddings):
-        self.user_manual_embeddings = user_manual_embeddings
-        self.release_notes_embeddings = release_notes_embeddings
+    def __init__(self, book_one_embeddings, book_two_embeddings):
+        self.book_one_embeddings = book_one_embeddings
+        self.book_two_embeddings = book_two_embeddings
 
     def route_query(self, query, embedding_similarity):
-        max_similarity_user_manual = embedding_similarity.compute_similarity(query, self.user_manual_embeddings)
-        max_similarity_release_notes = embedding_similarity.compute_similarity(query, self.release_notes_embeddings)
+        max_similarity_user_manual = embedding_similarity.compute_similarity(query, self.book_one_embeddings)
+        max_similarity_release_notes = embedding_similarity.compute_similarity(query, self.book_two_embeddings)
 
         if max_similarity_user_manual > max_similarity_release_notes:
             return "Book One"
