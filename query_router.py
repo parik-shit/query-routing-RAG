b753d2690daae8cb9d2943a021d8ab9e72265d32
Satyam Sharma <satyam17102003@gmail.com>
2024-08-18 13:37:59 +0530

    Add missing line to file

--- query_router.py
+++ query_router.py
@@ -4,10 +4,10 @@ class QueryRouter:
         self.book_two_embeddings = book_two_embeddings
 
     def route_query(self, query, embedding_similarity):
-        max_similarity_user_manual = embedding_similarity.compute_similarity(query, self.book_one_embeddings)
-        max_similarity_release_notes = embedding_similarity.compute_similarity(query, self.book_two_embeddings)
+        max_similarity_book_one = embedding_similarity.compute_similarity(query, self.book_one_embeddings)
+        max_similarity_book_two = embedding_similarity.compute_similarity(query, self.book_two_embeddings)
 
-        if max_similarity_user_manual > max_similarity_release_notes:
+        if max_similarity_book_one > max_similarity_book_two:
             return "Book One"
         else:
             return "Book Two"
